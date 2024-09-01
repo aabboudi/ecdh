cea1612b57e555572e08ee20c8d2942a9d20997e
aabboudi <abboudi.abdellah@gmail.com>
2024-09-01 02:57:13 +0100

    Initial commit

---
+++ components/footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer className="w-full flex items-center justify-center py-3">
+      <p>All rights reserved.</p>
+  </footer>
+  );
+};
---
+++ config/site.ts
@@ -0,0 +1,31 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "ECDH",
+  description: "Par les jeunes, pour les jeunes.",
+  navItems: [
+    {
+      label: "About",
+      href: "/about",
+    },
+    {
+      label: "Projects",
+      href: "/projects",
+    },
+    {
+      label: "Spotlight",
+      href: "/spotlight",
+    },
+    {
+      label: "Partners",
+      href: "/partners",
+    },
+  ],
+  links: {
+    github: "#",
+    twitter: "#",
+    docs: "#",
+    discord: "#",
+    sponsor: "#",
+  },
+};
