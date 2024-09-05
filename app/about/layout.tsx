49a157b1dc22aecb0aad6968016e133bcf285bac
aabboudi <abboudi.abdellah@gmail.com>
2024-09-05 02:18:54 +0100

    Cleaned up codebase

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -3,10 +3,5 @@ export default function AboutLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    // <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-    //   <div className="inline-block text-center justify-center">{children}</div>
-    // </section>
-    <main>{children}</main>
-  );
+  return <div className="pt-36">{children}</div>;
 }
