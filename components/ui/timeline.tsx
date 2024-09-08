d310a8752fac90bd5a247a01cda7fe9fadc77fbd
aabboudi <abboudi.abdellah@gmail.com>
2024-09-08 22:35:10 +0100

    Refactored Timeline component to dynamically generate project content

--- components/ui/timeline.tsx
+++ components/ui/timeline.tsx
@@ -1,10 +1,18 @@
 "use client";
 import { useScroll, useTransform, motion } from "framer-motion";
 import React, { useEffect, useRef, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 interface TimelineEntry {
+  dates: {
+    start: string;
+    end: string;
+  };
+  description: string;
+  images: string[];
+  link: string;
   title: string;
-  content: React.ReactNode;
 }
 
 export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
@@ -36,20 +44,37 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
       <div ref={ref} className="relative max-w-7xl mx-auto pb-20">
         {data.map((item, index) => (
           <div key={index} className="flex justify-start pt-10 md:gap-10">
-            <div className="sticky flex flex-col md:flex-row z-[4] items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
+            <div className="sticky flex flex-col md:flex-row z-[3] items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
               <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-white dark:bg-black flex items-center justify-center">
                 <div className="h-4 w-4 rounded-full bg-neutral-200 dark:bg-neutral-800 border border-neutral-300 dark:border-neutral-700 p-2" />
               </div>
-              <h3 className="hidden md:block text-xl md:pl-20 md:text-5xl font-bold text-neutral-500 dark:text-neutral-500 ">
-                {item.title}
+              <h3 className="hidden md:block text-xl md:pl-20 md:text-5xl font-bold text-neutral-500 dark:text-neutral-500 dark:hover:text-neutral-600">
+                <Link className="inline-flex md:text-5xl" href={item.link}>
+                  {item.title}
+                </Link>
               </h3>
             </div>
 
             <div className="relative pl-20 pr-4 md:pl-4 w-full">
-              <h3 className="md:hidden block text-2xl mb-4 text-left font-bold text-neutral-500 dark:text-neutral-500">
+              <h3 className="md:hidden block text-2xl mt-1 mb-4 text-left font-bold text-neutral-500 dark:text-neutral-500">
                 {item.title}
               </h3>
-              {item.content}{" "}
+              <div>
+                <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mt-4 mb-8">
+                  {item.description}
+                </p>
+                <div className="grid grid-cols-2 gap-4">
+                  {item.images.map((img: string) => (
+                    <Image
+                      alt="startup template"
+                      className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+                      height={500}
+                      src={img}
+                      width={500}
+                    />
+                  ))}
+                </div>
+              </div>
             </div>
           </div>
         ))}
